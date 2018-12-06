31328dea91dd85ff6b053e08e5ef45275e8f4ffd
Braedy Kuzma <braedy@ualberta.ca>
2018-12-06 14:23:55 -0700

    Add gene analysing.

---
+++ analyseGenes.py
@@ -0,0 +1,58 @@
+import itertools
+import numpy as np
+import pickle
+
+# Create names and info for producing mapping names.
+actionNames = ('move', 'right', 'left', 'mate', 'eat')
+stateNames = ('bias', 'energy', 'θs', 'Δs', 'θw', 'Δw', 'θg', 'Δg', 'pref', 'childAge')
+profileSize = 30
+
+# Produce name mappings for all of the weights.
+actionWeightNames = tuple('{}TO{}'.format(s, a) for s in stateNames for a in actionNames)
+valueWeightNames = tuple('{}TOval'.format(s) for s in stateNames)
+profileWeightNames = tuple('({})TO{}'.format(w, o)
+                           for w in itertools.chain(valueWeightNames, actionWeightNames)
+                           for o in range(profileSize))
+
+def getLastData(files):
+  # Extract last agent data from files.
+  data = []
+  for path in files:
+    with open(path, 'rb') as f:
+      # Apparently some files didn't finish dumping.
+      try:
+        genes = pickle.load(f)
+      except EOFError:
+        print('{} was malformed, skipping.'.format(path))
+
+      # Get last agent.
+      lastAgent = max(genes.keys())
+      lastData = genes[lastAgent]
+
+      # Sanity check.
+      assert len(lastData['initial_action_net'].flat) == len(actionWeightNames)
+      assert len(lastData['evaluation_net'].flat) == len(valueWeightNames)
+      assert len(lastData['profile_net'].flat) == len(profileWeightNames)
+
+      data.append(lastData)
+      
+  return data
+
+def rankPreferences(data):
+  # First index is a weight, second index is the number of times that it appeared in that
+  # place in the sorted absolute weights.
+  n = len(profileWeightNames)
+  counts = np.zeros((n, n), dtype=np.int32)
+
+  for d in data:
+    prof = d['profile_net'].flat
+    prof = np.abs(prof)
+    inds = np.argsort(-prof) # Make all values negative in the arg sort for reverse order
+
+    # The wth weight was sorted into the ith position.
+    for i, w in enumerate(inds):
+      counts[w, i] += 1
+
+  cInds = np.lexsort(np.rot90(-counts))
+  for i in cInds[:10]:
+    print('Rank: {}, Name: {}, Count: {}'.format(i, profileWeightNames[i], counts[i][:10]))
---
+++ ganalyse.py
@@ -0,0 +1,27 @@
+import os
+import sys
+
+import analyseGenes as ag
+import analyseUtil as util
+
+if __name__ == '__main__':
+  argc = len(sys.argv)
+
+  if argc <= 1:
+    print('Missing arguments.')
+    sys.exit(0)
+  
+  files = []
+  for path in sys.argv[1:]:
+    if os.path.isdir(path):
+      files.extend(util.gatherPcks(path))
+    elif os.path.isfile(path) and os.path.splitext(path)[1] == '.pck':
+      files.append(path)
+    else:
+      print('Couldn\'t figure out {}'.format(path))
+
+  print('Found {} files to analyse.'.format(len(files)))
+
+  data = ag.getLastData(files)
+
+  ag.rankPreferences(data)
